c89ec94ed17e3565e2cbba7a29829cfac4f4a870
tanmaymp <tmparulekar@gmail.com>
2023-10-15 03:49:18 -0700

    all interfaces implemented

--- Customer.py
+++ Customer.py
@@ -1,9 +1,9 @@
-import logging
 import grpc
 import bank_pb2
 import bank_pb2_grpc
-import time
 import consts
+import multiprocessing
+import json
 
 class Customer:
     def __init__(self, id, events):
@@ -19,48 +19,16 @@ class Customer:
     # TODO: students are expected to create the Customer stub
     def createStub(self):
         port = consts.BASE_PORT+self.id
-        # channel = grpc.insecure_channel('localhost:{}'.format(port))
         channel = grpc.insecure_channel('localhost:{}'.format(port))
         stub = bank_pb2_grpc.BankStub(channel)
         self.stub = stub
         # print("stub on ",port)
         return stub
-    '''
-    # TODO: students are expected to send out the events to the Bank
-    def executeEvents(self):
-        output={"id":self.id, "recv":[]}
-        print("Executing customer object , id {}".format(self.id))
-        stub = self.createStub()
-        # self.createStub()
-        # print(stub)s
-        for event in self.events:
-            print("Executing event {}".format(event))
-            if event['interface'] == 'query':
-                request = bank_pb2.CustomerQueryRequest(id = int(self.id))
-                response = stub.BranchQuery(request)
-                output['recv'].append({"interface":"query", "balance":response.msg})
-            elif event['interface'] == 'deposit':
-                request = bank_pb2.CustomerTransactRequest(id = int(self.id), money=int(event['money']))
-                response = stub.BranchDeposit(request)
-                output['recv'].append({"interface":"deposit", "result":response.msg})
-
-            elif event['interface'] == 'withdraw':
-                request = bank_pb2.CustomerTransactRequest(id = int(self.id), money=int(event['money']))
-                response = stub.BranchWithdraw(request)
-                output['recv'].append({"interface":"deposit", "result":response.msg})
-            else:
-                logging.error("Invalid interface defined for event")
         
-        return output
-    '''
     def executeEvents(self):
         output={"id":self.id, "recv":[]}
-        print("Executing customer object , id {}".format(self.id))
         stub = self.createStub()
-        # self.createStub()
-        # print(stub)s
         for event in self.events:
-            print("Executing event {}".format(event))
             type = 'customer'
             balance = 0
             if('money' in event.keys()):
@@ -69,22 +37,23 @@ class Customer:
                 money = 0
             request = bank_pb2.BankRequest(id = int(self.id), type='customer', interface=event['interface'], balance=balance, money=money)
             response = stub.MsgDelivery(request)
-            '''
-            if event['interface'] == 'query':
-                request = bank_pb2.CustomerQueryRequest(id = int(self.id))
-                response = stub.BranchQuery(request)
-                output['recv'].append({"interface":"query", "balance":response.msg})
-            elif event['interface'] == 'deposit':
-                request = bank_pb2.CustomerTransactRequest(id = int(self.id), money=int(event['money']))
-                response = stub.BranchDeposit(request)
-                output['recv'].append({"interface":"deposit", "result":response.msg})
-
-            elif event['interface'] == 'withdraw':
-                request = bank_pb2.CustomerTransactRequest(id = int(self.id), money=int(event['money']))
-                response = stub.BranchWithdraw(request)
-                output['recv'].append({"interface":"deposit", "result":response.msg})
-            else:
-                logging.error("Invalid interface defined for event")
-            '''
-            output['recv'].append({"interface":response.interface, "result":response.result, "balance":response.balance})
+            if response.interface in ('deposit', 'withdraw'):
+                resp = {"interface":response.interface, "result":response.result}
+            elif response.interface == 'query':
+                resp = {"interface":response.interface, "balance":response.balance}
+            output['recv'].append(resp)
         return output
+    
+def execute_customer_processes(id, events):
+    cust_obj = Customer(id=id, events=events)
+    #print("\nExecuting customer events")
+    output_obj = cust_obj.executeEvents()
+    with open(consts.OUTPUT_FILE, 'a') as file:
+        json.dump(output_obj, file)
+        file.write('\n')
+
+def initialize_customer_processes(customer_list):
+    for customers in customer_list:
+        cust_process = multiprocessing.Process(target=execute_customer_processes, args=(customers['id'], customers['events']))
+        cust_process.start()
+        cust_process.join()
